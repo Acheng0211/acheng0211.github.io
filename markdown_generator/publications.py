743a25990606f245eb9776f4fc46ab1c9df42fc9
Acheng0211 <12111820@mail.sustech.edu.cn>
2024-11-10 14:27:51 +0800

    fix: publications citations

--- markdown_generator/publications.py
+++ markdown_generator/publications.py
@@ -88,7 +88,7 @@ for row, item in publications.iterrows():
     if len(str(item.paper_url)) > 5:
         md += "\npaperurl: '" + item.paper_url + "'"
     
-    md += "\ncitation: '" + html_escape(item.citation) + "'"
+    # md += "\ncitation: '" + html_escape(item.citation) + "'"
     
     md += "\n---"
     
@@ -100,7 +100,7 @@ for row, item in publications.iterrows():
     if len(str(item.excerpt)) > 5:
         md += "\n" + html_escape(item.excerpt) + "\n"
         
-    # md += "\nRecommended citation: " + item.citation
+    md += "\nRecommended citation: " + item.citation
     
     md_filename = os.path.basename(md_filename)
        
